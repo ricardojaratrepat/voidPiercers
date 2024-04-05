86106fa682f0ca1cb76832efb7040df7cb7399fa
Eduardo Cilveti <eacilveti@miuandes.cl>
2024-04-05 19:52:46 -0300

    terrain generation

---
+++ Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour
+{
+    public float surfaceValue = 0.25f;
+    public Sprite tile;
+    public int worldSize = 100;
+    public float caveFreq = 0.05f;
+    public float terrainFreq = 0.05f;
+    public float heightMultiplier = 4f;
+    public int heightAddition = 25;
+    public float seed;
+
+    public Texture2D noiseTexture;
+
+    private void Start()
+    {
+        seed = Random.Range(-10000, 10000);
+        GenerateNoiseTexture();
+        GenerateTerrain();
+    }
+
+    public void GenerateTerrain()
+    {
+        for (int x = 0; x < worldSize; x++)
+        {
+            float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
+            for (int y = 0; y < height; y++)
+            {
+                if (noiseTexture.GetPixel(x, y).r > surfaceValue)
+                {
+                    GameObject newTile = new GameObject(name = "tile");
+                    newTile.transform.parent = this.transform;
+                    newTile.AddComponent<SpriteRenderer>();
+                    newTile.GetComponent<SpriteRenderer>().sprite = tile;
+                    newTile.transform.position = new Vector2(x + 0.5f, y+0.5f);
+                }
+            }
+        }
+    }
+    public void GenerateNoiseTexture()
+    {
+        noiseTexture = new Texture2D(worldSize, worldSize);
+        for (int x = 0; x < noiseTexture.width; x++)
+        {
+            for (int y = 0; y < noiseTexture.height; y++)
+            {
+                float v = Mathf.PerlinNoise((x + seed) * caveFreq,(y + seed)* caveFreq);
+                noiseTexture.SetPixel(x, y, new Color(v, v, v));
+            }
+        }
+        noiseTexture.Apply();
+    }
+}
