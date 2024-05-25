98a217b445aaade02e7badfd4cb7f3c355abab84
Mstrucco <mstrucco@miuandes.cl>
2024-05-25 14:01:45 -0400

    Generacion de terreno terminada
    Hice la generacion de los 10 minerales que nos pidieron, hice sprites para cada uno y decidi arbitrariamente alturas en las que generarlos. Puse sliders para customizar la generacion de los minerales, para probar distintas configuraciones editen el prefab de terreno grande, eso si tengan cuidado que los valores de cada mineral sean diferentes osino se van a generar en el mismo lugar. Cambie un poco la escena de sample para que se pueda usar con el terreno nuevo

---
+++ Assets/Scripts/OreClass.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using UnityEngine;
+[System.Serializable]
+public class OreClass
+{
+    public string name;
+    [Range(0,1)]
+    public float rarity;
+    [Range(0, 1)]
+    public float size;
+    public int maxSpawnHeight;
+    public Texture2D spreadTexture;
+}
