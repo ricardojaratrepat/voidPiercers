5586d3241d50c0665fc691554a18c19570e729e5
Vicente Cuevas Lavín <101298892+VicenteCuevasL@users.noreply.github.com>
2024-06-09 22:46:50 -0400

    Solucion bug BatController.cs

--- Assets/Scripts/Enemies/BatEnemy/BatController.cs
+++ Assets/Scripts/Enemies/BatEnemy/BatController.cs
@@ -74,6 +74,15 @@ public class BatController : MonoBehaviour
         }
     }
 
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject == Player)
+        {
+            Vector2 pushDirection = new Vector2(1f, 0f);
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(pushDirection, ForceMode2D.Impulse);
+        }
+    }
+
     void ApplyKnockback(Vector2 knockbackDirection)
     {
         if (!isKnockbackActive)
