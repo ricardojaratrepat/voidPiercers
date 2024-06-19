fa5b9e880e438756f4ceb9efdf9c6321f34c99ef
EduardoCilvetiUA <eacilveti@miuandes.cl>
2024-06-19 14:22:22 -0400

    feat: Add light bulb timer functionality to DarkCircleController

--- Assets/Scripts/DarkCircle/DarkCircleController.cs
+++ Assets/Scripts/DarkCircle/DarkCircleController.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DarkCircleController : MonoBehaviour
 {
     private PulseEffect pulseEffect;
+    public GameObject lightBulbCanvas;
+    private Image lightBulbImage;
+    private float timer;
+    private Text timerText;
+    private bool timerActive = false;
 
     void Start()
     {
+        lightBulbImage = lightBulbCanvas.GetComponentInChildren<Image>();
+        timerText = lightBulbCanvas.transform.Find("Timer").GetComponent<Text>();
         pulseEffect = GetComponent<PulseEffect>();
         if (pulseEffect == null)
         {
@@ -21,15 +29,35 @@ public class DarkCircleController : MonoBehaviour
 
     IEnumerator EnlargeCoroutine()
     {
-        if (pulseEffect != null)
+        if (pulseEffect != null && lightBulbImage != null)
         {
+            // Activar el canvas
+            lightBulbCanvas.SetActive(true);
+
             // Aumenta el factor de escala al doble
             pulseEffect.scaleFactor = 2f;
 
-            // Espera 20 segundos
-            yield return new WaitForSeconds(20);
+            // Inicializar el tiempo de espera
+            float waitTime = 20f;
 
-            // Restaura el factor de escala original
+            // Mientras hay tiempo de espera
+            while (waitTime > 0)
+            {
+                // Actualizar el texto del temporizador
+                timerText.text = waitTime.ToString();
+
+                // Esperar 1 segundo
+                yield return new WaitForSeconds(1f);
+
+                Debug.Log("Tiempo restante: " + waitTime);
+                // Reducir el tiempo de espera
+                waitTime--;
+            }
+
+            // Desactivar el canvas
+            lightBulbCanvas.SetActive(false);
+
+            // Restaurar el factor de escala original
             pulseEffect.scaleFactor = 1f;
         }
     }
